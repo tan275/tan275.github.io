feeef719f537278adb0afb6f7f0a818ba58fc7fd
tania <taniamuley27@gmail.com>
2023-06-18 23:07:24 +0530

    commit 9

---
+++ src/animate.js
@@ -0,0 +1,23 @@
+const texts = ["create like an artist, solve like an engineer."];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+var speed=80;
+
+(function type(){
+
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, speed);
+})();
+
